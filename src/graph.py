d1b10e4df083d12a0a08745559b246c79e486b11
Guilherme <guiegle@hotmail.com>
2023-06-09 11:03:41 -0300

    leitura de arquivos

--- src/graph.py
+++ src/graph.py
@@ -1,7 +1,23 @@
 class Graph():
-    def __init__(self, nodes, edges) -> None:
+
+    def __init__(self) -> None:
+        pass
+
+    def __init__(self, nodes : list, edges: list) -> None:
         self.nodes = nodes
         self.edges = edges
 
     def get_degree(node):
+        pass
+
+    def add_node(node):
+        pass
+
+    def remove_node(node):
+        pass
+
+    def add_edge(edge):
+        pass
+
+    def remove_edge(edge):
         pass
--- src/main.py
+++ src/main.py
@@ -1,5 +1,22 @@
+from sys import argv
+from pandas import read_csv
+from graph import Graph
+
 def chinese_postman_problem_solver():
     pass
 
+def read_file_to_graph(path):
+    file = open(path, 'r')
+    lines = [line.rstrip() for line in file]
+    file.close()
+    nodes = [int(element) for element in lines[:lines.index('#')]]
+    edges = [ [ int(e) for e in element.split(' ')] for element in lines[lines.index('#') +1 :]]
+    return Graph(nodes, edges)
+
 if __name__ == "__main__":
+    graphs = [read_file_to_graph(file_path) for file_path in argv[1:]]
+    for graph in graphs:
+        print(graph.edges)
+
+    pass
     chinese_postman_problem_solver()
